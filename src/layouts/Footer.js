f069d921894b021cd00e3dd8b3f6bf79037b0e84
Huishi <anhuishi95@gmail.com>
2023-01-14 13:04:02 -0800

    Update footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -10,22 +10,22 @@ const Footer = () => {
                 data-animate="active"
               >
                 <a target="_blank" rel="nofollow" href="https://www.linkedin.com/in/huishi-an-8397311b1/">
-                  <img src="https://img.shields.io/badge/LinkedIn-0A66C2?logo=linkedin&logoColor=fff&style=for-the-badge" alt="linkedin"></img>
+                  <i className="fa-brands fa-linkedin"></i>
                 </a>
                 <a target="_blank" rel="nofollow" href="https://github.com/huishi329">
-                  <img src="https://img.shields.io/badge/GitHub-181717?logo=github&logoColor=fff&style=for-the-badge" alt="github"></img>
+                  <i className="fa-brands fa-github"></i>
                 </a>
                 <a target="_blank" rel="nofollow" href="mailto:anhuishi95@gmail.com">
-                  <img src="https://img.shields.io/badge/Gmail-EA4335?logo=gmail&logoColor=fff&style=for-the-badge" alt="gmail"></img>
+                  <i className="fa-solid fa-envelope"></i>
                 </a>
               </div>
             </div>
             <div className="col-xs-12 col-sm-12 col-md-4 col-lg-4">
               <div
-                className="copyright-text align-center scrolla-element-anim-1 scroll-animate"
+                className=" align-center scrolla-element-anim-1 scroll-animate"
                 data-animate="active"
               >
-                © 2023 <strong> Huishi An </strong>. All rights reserved
+                <strong> Huishi An </strong>is open to volunteer and open source work
               </div>
             </div>
           </div>
